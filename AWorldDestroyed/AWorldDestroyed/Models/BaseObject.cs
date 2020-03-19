ef10e3e61e75f56fd37198126a566c6e0a5d49b6
Daniel Andulahad <daniel00.abdulahad@gmail.com>
2020-03-20 00:09:24 +0100

    Added fishes and birbs :)

--- AWorldDestroyed/AWorldDestroyed/Models/BaseObject.cs
+++ AWorldDestroyed/AWorldDestroyed/Models/BaseObject.cs
@@ -1,4 +1,17 @@
-﻿namespace AWorldDestroyed.Models
+﻿// =============================================
+//         Editor:     Daniel Abdulahad
+//         Last edit:  2020-03-18 
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
+//       (\                 >+{{{o)> - kvaouk
+//    >+{{{{{0)> - kraouk      LL  
+//       /_\_
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                <333333><                     
+//         <3333333><           <33333>< 
+
+namespace AWorldDestroyed.Models
 {
     /// <summary>
     /// Base class for all objects.
@@ -14,8 +27,7 @@
         /// <summary>
         /// Initialize a new BaseObject with a default name.
         /// </summary>
-        public BaseObject()
-            : this($"Object{id}")
+        public BaseObject() : this($"Object{id}")
         {
         }
 
