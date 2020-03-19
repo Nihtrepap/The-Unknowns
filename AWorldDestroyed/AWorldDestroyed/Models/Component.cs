31749b89bba81ad230a5cc7150295d591ec24faa
LoneMaaherra <lonemaaherra@gmail.com>
2020-03-19 10:31:58 +0100

    Add Component class.

---
+++ AWorldDestroyed/AWorldDestroyed/Models/Component.cs
@@ -0,0 +1,28 @@
+﻿// =============================================
+//         Editor:     Lone Maaherra
+//         Last edit:  2020-03-19 
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
+//       (\                 >+{{{o)> - kvaouk
+//    >+{{{{{0)> - kraouk      LL  
+//       /_\_
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                <333333><                     
+//         <3333333><           <33333>< 
+
+namespace AWorldDestroyed.Models
+{
+    /// <summary>
+    /// Base class for all components.
+    /// </summary>
+    class Component : BaseObject
+    {
+        public GameObject GameObject { get; protected set; }
+
+        public Component(GameObject gameObject)
+        {
+            GameObject = gameObject;
+        }
+    }
+}
