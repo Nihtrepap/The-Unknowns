2b074685ded53f4b10874061a3cac8946747dd7f
Daniel Andulahad <daniel00.abdulahad@gmail.com>
2020-03-19 16:08:53 +0100

    Added signature

--- AWorldDestroyed/AWorldDestroyed/Tag.cs
+++ AWorldDestroyed/AWorldDestroyed/Tag.cs
@@ -1,4 +1,17 @@
-﻿namespace AWorldDestroyed
+﻿// =============================================
+//         Editor:     Daniel Abdulahad
+//         Last edit:  2020-03-19
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
+//       (\                 >+{{{o)> - kvaouk
+//    >+{{{{{0)> - kraouk      LL  
+//       /_\_
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                <333333><                     
+//         <3333333><           <33333><
+
+namespace AWorldDestroyed
 {
     /// <summary>
     /// Marker values that you can use to identify objects.
