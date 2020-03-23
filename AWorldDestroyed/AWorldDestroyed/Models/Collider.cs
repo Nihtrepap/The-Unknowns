25c47d349f064b0aeb00f0c0de4d2c1f8141adbb
Philip <philips.abrahamsson@gmail.com>
2020-03-23 23:47:05 +0100

    Going to fix it more

---
+++ AWorldDestroyed/AWorldDestroyed/Models/Collider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWorldDestroyed.Models
+{
+    /// <summary>
+    /// Check if objects are colliding.
+    /// </summary>
+    class Collider : Component
+    {
+        public Vector2 Size { get; set; }
+        public Vector2 Offset { get; set; }
+
+        public Collider(SceneObject sceneObject, Rectangle size) :base(sceneObject)
+        {
+        }
+
+        public override Component Copy()
+        {
+            return this;
+        }
+    }
+}
