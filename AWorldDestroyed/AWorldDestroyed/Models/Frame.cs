ecdd04392bafe8f1734a4311c2a06e401d318027
LoneMaaherra <lonemaaherra@gmail.com>
2020-03-24 11:16:38 +0100

    Cleanup Frame struct.

--- AWorldDestroyed/AWorldDestroyed/Models/Frame.cs
+++ AWorldDestroyed/AWorldDestroyed/Models/Frame.cs
@@ -1,19 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// =============================================
+//         Editor:     Philip  Abrahamsson
+//         Last edit:  2020-03-24 
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
+//       (\                 >+{{{o)> - kvaouk
+//    >+{{{{{0)> - kraouk      LL  
+//       /_\_
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                <333333><                     
+//         <3333333><           <33333>< 
 
 namespace AWorldDestroyed.Models
 {
     /// <summary>
-    /// Frame is a structure with Sprite.
-    /// And will be used as a container for sprite sheets.
+    /// A single frame of an animation.
     /// </summary>
     public struct Frame
     {
-        public Sprite Sprite { get; set; }
-        public int Duration { get; set; }
-        public delegate void Event();
+        public readonly Sprite Sprite;
+        public readonly int Duration;
+        public event FrameEvent Event;
+
+        /// <summary>
+        /// Create a new Frame with given Sprite and duration.
+        /// </summary>
+        /// <param name="sprite">The Sprite of the frame.</param>
+        /// <param name="duration">The duration of the frame.</param>
+        public Frame(Sprite sprite, int duration)
+        {
+            Sprite = sprite;
+            Duration = duration;
+            Event = null;
+        }
+
+        /// <summary>
+        /// Invoke the Event attached to this Frame.
+        /// </summary>
+        public void InvokeEvent()
+        {
+            Event?.Invoke();
+        }
     }
+
+    public delegate void FrameEvent();
 }
