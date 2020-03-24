20b92ee08eacd92d133a347dfdf5fc143e5b1df1
LoneMaaherra <lonemaaherra@gmail.com>
2020-03-24 10:27:28 +0100

    Cleanup SpriteRenderer class.

--- AWorldDestroyed/AWorldDestroyed/Models/SpriteRenderer.cs
+++ AWorldDestroyed/AWorldDestroyed/Models/SpriteRenderer.cs
@@ -11,14 +11,14 @@
 //                <333333><                     
 //         <3333333><           <33333>< 
 
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace AWorldDestroyed.Models
 {
     /// <summary>
-    /// This is used to display sprites.
-    /// What layer to display them on.
+    /// Determines how to render a sprite.
     /// </summary>
     public class SpriteRenderer : Component
     {
@@ -26,20 +26,34 @@ namespace AWorldDestroyed.Models
         public Color Color { get; set; }
         public int SortingOrder { get; set; }
         public SortingLayer SortingLayer { get; set; }
-        public SpriteEffect SpriteEffect { get; set; }
+        public SpriteEffects SpriteEffect { get; set; }
 
-        public SpriteRenderer(GameObject gameObject, Sprite sprite)
+        /// <summary>
+        /// Create a new SpriteRenderer instance.
+        /// </summary>
+        /// <param name="sprite"></param>
+        public SpriteRenderer(Sprite sprite)
         {
             Sprite = sprite;
+            Color = Color.White;
+            SortingOrder = 0;
+            SortingLayer = Enum.Parse(SortingLayer, "default");
+            SpriteEffect = SpriteEffects.None;
         }
 
         /// <summary>
-        /// Creates a copy of SpriteRenderer object.
+        /// Make a new SpriteRenderer instance with the same attribute values as this instance.
         /// </summary>
         /// <returns>SpriteRenderer object</returns>
-        public object Copy()
+        public override Component Copy()
         {
-            return this.MemberwiseClone();
+            return new SpriteRenderer(this.Sprite)
+            {
+                Color = this.Color,
+                SortingOrder = this.SortingOrder,
+                SortingLayer = this. SortingLayer,
+                SpriteEffect = this.SpriteEffect
+            };
         }
     }
 }
