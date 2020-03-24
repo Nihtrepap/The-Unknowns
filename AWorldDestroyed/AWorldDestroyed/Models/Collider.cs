194143194b4f8ec4a010aed12d78df4b16d2a34a
Philip <philips.abrahamsson@gmail.com>
2020-03-24 15:38:13 +0100

    Made the collider.

--- AWorldDestroyed/AWorldDestroyed/Models/Collider.cs
+++ AWorldDestroyed/AWorldDestroyed/Models/Collider.cs
@@ -1,27 +1,58 @@
-﻿using Microsoft.Xna.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// =============================================
+//         Editor:     Philip  Abrahamsson
+//         Last edit:  2020-03-24
+// _-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+//
+//       (\                 >+{{{o)> - kvaouk
+//    >+{{{{{0)> - kraouk      LL  
+//       /_\_
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                <333333><                     
+//         <3333333><           <33333>< 
+
+using Microsoft.Xna.Framework;
 
 namespace AWorldDestroyed.Models
 {
     /// <summary>
-    /// Check if objects are colliding.
+    /// Define bounds for collision detection.
     /// </summary>
     class Collider : Component
     {
         public Vector2 Size { get; set; }
         public Vector2 Offset { get; set; }
 
-        public Collider(SceneObject sceneObject, Rectangle size) :base(sceneObject)
+        /// <summary>
+        /// Creates a collider.
+        /// </summary>
+        /// <param name="size">Size.</param>
+        public Collider(Vector2 size) :base()
         {
+            Size = size;
+            Offset = Vector2.Zero;
         }
 
+        /// <summary>
+        /// Get the bounds rectangle of this collider.
+        /// </summary>
+        /// <returns>Floating point 2D-rectangle.</returns>
+        public RectangleF GetRectangle()
+        {
+            Vector2 position = SceneObject.Transform.Position + Offset;
+            return new RectangleF(position, Size);
+        }
+
+        /// <summary>
+        /// Get a copy of this collider.
+        /// </summary>
+        /// <returns>Collider copy.</returns>
         public override Component Copy()
         {
-            return this;
+            return new Collider(this.Size)
+            {
+                Offset = this.Offset
+            };
         }
     }
 }
