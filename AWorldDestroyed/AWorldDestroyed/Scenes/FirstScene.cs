294d5b9a8c467f3ce60e2fbc8cda5f7690df31f7
LoneMaaherra <lonemaaherra@gmail.com>
2020-04-10 09:16:37 +0200

    Remove using statements in first scene.

--- AWorldDestroyed/AWorldDestroyed/Scenes/FirstScene.cs
+++ AWorldDestroyed/AWorldDestroyed/Scenes/FirstScene.cs
@@ -1,15 +1,10 @@
 ﻿using AWorldDestroyed.GameObjects;
 using AWorldDestroyed.Map;
 using AWorldDestroyed.Models;
-using AWorldDestroyed.Models.Components;
-using AWorldDestroyed.Scripts;
 using AWorldDestroyed.Utility;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using System.IO;
-using System.Xml.Linq;
-using System.Xml.Serialization;
 
 namespace AWorldDestroyed.Scenes
 {
@@ -79,26 +74,14 @@ namespace AWorldDestroyed.Scenes
                     SpriteBatch.Draw(Game1.Pixel, new Rectangle(enemy.Transform.Position.ToPoint(), new Point(4)), null, Color.Black, 0f, Vector2.Zero, SpriteEffects.None, sortingOrder + 0.0001f);
                 }
             }
-            //if (gameObject.HasComponent<RigidBody>())
-            //{
-            //    RectangleF objColRange = gameObject.GetComponent<Collider>().GetRectangle();
-            //    RectangleF col = new RectangleF(
-            //                objColRange.X - (2 * 32),
-            //                objColRange.Y - (2 * 32),
-            //                objColRange.Width + (4 * 32),
-            //                objColRange.Height + (4 * 32));
-
-            //    SpriteBatch.Draw(Game1.Pixel, (Rectangle)col, null, Color.BlueViolet * 0.6f, 0f, Vector2.Zero, SpriteEffects.None, sortingOrder);
-            //}
         }
         /// <summary>
         /// Here we set what to happen with GUI on draw.
         /// </summary>
         protected override void OnGUIDraw()
         {
-            float healthWidth = (p.Health / p.MaxHealth) * 700f;
+            float healthWidth = (p.Health / p.MaxHealth) * 200f;
             Rectangle healthBar = new Rectangle(15, 15, (int)healthWidth, 20);
-            //SpriteBatch.Draw(Game1.Pixel, new Rectangle(0, 0, 800, 30), null, Color.Orange);
             SpriteBatch.Draw(Game1.Pixel, healthBar, null, Color.Red);
 
             base.OnGUIDraw();
